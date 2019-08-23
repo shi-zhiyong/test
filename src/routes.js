762ae9731da266d369e1b27386d81fd4c0adb794
szy <503305354@qq.com>
2019-08-23 18:18:51 +0800

    mod

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 const Feed = resolve => require(['./views/feed.vue'], resolve);
-const Feed1 = resolve => require(['./views/feed1.vue'], resolve);
 const Foo = resolve => require(['./views/foo.vue'], resolve);
 const Cascader = resolve => require(['./views/cascader.vue'], resolve);
 
@@ -14,12 +13,6 @@ const routes = [
         path: '/feed',
         meta: {title: 'title-feed'},
         component: Feed
-    },
-    {
-        name: 'feed1',
-        path: '/feed1',
-        meta: {title: 'title-feed1'},
-        component: Feed1
     },
     {
         name: 'cascader',
--- src/views/feed.vue
+++ src/views/feed.vue
@@ -1,29 +1,60 @@
 <template>
-    <div class="page-feed" ref="feed">
-        <button ref="feed-btn" @click="tos">TOST</button>
-        <v-cell title="title" label="label-title" isLink :to="{path:'/foo#omg'}"></v-cell>
-        <v-cell title="baidu" label="label-title" isLink to="https://www.baidu.com"></v-cell>
-        <v-cell title="baidu" label="label-title" isLink to="https://www.baidu.com">
-            <div slot="right">删除</div>
+    <div class="page-feed"
+         ref="feed">
+        <button ref="feed-btn"
+                @click="tos">
+            TOST
+        </button>
+        <v-cell title="title"
+                label="label-title"
+                isLink
+                :to="{path:'/foo#omg'}"></v-cell>
+        <v-cell title="baidu"
+                label="label-title"
+                isLink
+                to="https://www.baidu.com"></v-cell>
+        <v-cell title="baidu"
+                label="label-title"
+                isLink
+                to="https://www.baidu.com">
+            <div slot="right">
+                删除
+            </div>
         </v-cell>
-        <feed1></feed1>
     </div>
 </template>
 
 <script>
-    import feed1 from './feed1'
-    import {Tost,vCell} from 'comb-ui'
+    import {
+        Tost,
+        vCell
+    } from 'comb-ui'
+
     export default {
         name: "feed",
-        components: {vCell,feed1},
-        data(){
+        components: {vCell},
+        data() {
             return {
-                feed:'feed'
+                feed: 'feed'
             }
         },
         customOption1: 'feed-omg',
+        created(){
+            setTimeout(this.fn,0);
+            this.$nextTick(function () {
+               this.fn('nexttick')
+            })
+        },
+        mounted() {
+            // let dom = document.querySelector('.page-feed');
+            // console.log('dom',dom);
+        },
         methods: {
-            tos(){
+            fn(a='timeout'){
+                let dom = document.querySelector('.page-feed');
+                console.log(a,dom);
+            },
+            tos() {
                 // alert('tost');
                 // const resolved = this.$router.match('feed?name=321#123');
                 // console.log(resolved,this.$el);
